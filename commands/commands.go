fd915b0b55cc1ea1cead385ea445e46ed191de54
Lintao <lintao.amons@outlook.com>
2023-03-12 13:51:47 +0800

    move commands out of main package

---
+++ commands/commands.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/LintaoAmons/nric-go/nric"
+	"github.com/atotto/clipboard"
+	"github.com/urfave/cli/v2"
+)
+
+var NricAction = func(cCtx *cli.Context) error {
+	var i int
+	i, err := strconv.Atoi(cCtx.Args().Get(0))
+	if err != nil {
+		i = 1
+	}
+	count := i
+	nricsString := nric.GenerateNricMultiple(count)
+	clipboard.WriteAll(nricsString)
+	fmt.Println(nricsString)
+	return nil
+}
+
+var NricCommand = &cli.Command{
+	Name:            "nric",
+	Description:     "A tool to generate testing Singapore NRIC number, and it will automaticlly copy into you clipboard",
+	HideHelpCommand: true,
+	UsageText:       "nric [count] -- count is set to 1 if not passed",
+	Usage:           "nric [count]",
+	Action:          NricAction,
+}
--- main.go
+++ main.go
@@ -1,37 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strconv"
 
-	"github.com/LintaoAmons/nric-go/nric"
-	"github.com/atotto/clipboard"
+	"github.com/LintaoAmons/nric-go/commands"
 	"github.com/urfave/cli/v2"
 )
-
-var NricAction = func(cCtx *cli.Context) error {
-	var i int
-	i, err := strconv.Atoi(cCtx.Args().Get(0))
-	if err != nil {
-		i = 1
-	}
-	count := i
-	nricsString := nric.GenerateNricMultiple(count)
-	clipboard.WriteAll(nricsString)
-	fmt.Println(nricsString)
-	return nil
-}
-
-var NricCommand = &cli.Command{
-	Name:            "nric",
-	Description:     "A tool to generate testing Singapore NRIC number, and it will automaticlly copy into you clipboard",
-	HideHelpCommand: true,
-	UsageText:       "nric [count] -- count is set to 1 if not passed",
-	Usage:           "nric [count]",
-	Action:          NricAction,
-}
 
 func main() {
 	app := &cli.App{
@@ -40,7 +15,7 @@ func main() {
 		HideHelpCommand: true,
 		UsageText:       "nric [count] -- count is set to 1 if not passed",
 		Usage:           "nric [count]",
-		Action:          NricAction,
+		Action:          commands.NricCommand.Action,
 	}
 
 	if err := app.Run(os.Args); err != nil {
